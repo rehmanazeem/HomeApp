953efdc5d4bc579c24a579327862c95bb02f48a8
Azeem Ur Rehman <rehmanazeem915@gmail.com>
2025-08-30 22:10:25 +0200

    Initial Commit

---
+++ mqtt_secrets.js
@@ -0,0 +1,3 @@
+export const MQTT_BROKER = 'wss://d87d1628bac24e73948029077297f61e.s1.eu.hivemq.cloud:8884/mqtt';
+export const MQTT_USER = 'HomeEspServer';
+export const MQTT_PASS = 'zamnag-sinwim-bUrvy4';
